36438d44c58d843d2324e67666ade40bfc5c9021
Faruk BALCI <farukbalci99@hotmail.com>
2019-07-31 16:39:33 +0300

    Checkpoint2 - not working

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import MyComponent from './components/MyComponent'
+import MyDataComponent from './components/MyDataComponent'
 
 function App() {
 	
 
   return (
     <div className="App">
+    	<MyDataComponent></MyDataComponent>
     	<MyComponent name = "Faruk" >cocuk bir</MyComponent>
     	<MyComponent name = "okan" abc = "12d3">çocuk iki</MyComponent>
     </div>
