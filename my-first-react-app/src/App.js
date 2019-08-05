51f62219337ab4b2c6c7b6dec7b5cb08e455935a
Faruk BALCI <farukbalci99@hotmail.com>
2019-08-05 16:06:53 +0300

    Finish form

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import MyComponent from './components/MyComponent'
-import MyDataComponent from './components/MyDataComponent'
 import MyForm from './components/form'
 
 function App() {
@@ -9,7 +7,6 @@ function App() {
 
   return (
     <div className="App">
-    	<MyDataComponent></MyDataComponent>
     	<MyForm></MyForm>
     </div>
   );
--- my-first-react-app/src/components/form.js
+++ my-first-react-app/src/components/form.js
@@ -1,30 +1,39 @@
-import React, {Component} from 'react';
-import Form from 'react-bootstrap/Form';
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./form.css";
 
 class MyForm extends Component {
+  render() {
+    return (
+      <div style={{ border: "1px solid grey", width: 500 }}>
+        <Form>
+          <Form.Group controlId="Adi">
+            <Form.Label>Adi</Form.Label>
+            <Form.Control type="string" placeholder="Adini Girin" />
+          </Form.Group>
 
-	render() {
-		return <div>
-		    <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group controlId="formBasicChecbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-
-            </Form>
-		</div>
-	}
+          <Form.Group controlId="URL">
+            <Form.Label>URL</Form.Label>
+            <Form.Control type="url" placeholder="URL" />
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>HTTP Methodu</Form.Label>
+            <Form.Control as="select">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Form.Control>
+          </Form.Group>
+          <Button variant="primary" type="Ekle">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default MyForm;
